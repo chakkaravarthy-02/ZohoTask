b0577d6a3014db10f94dd03135a33a5415c1ec35
chakkaravarthy-02 <saranchakkaravarthy@gmail.com>
2024-07-04 21:27:00 +0530

    Array-2 added

---
+++ Task-1/Java/Array-2/haveThree.java
@@ -0,0 +1,25 @@
+/*
+
+Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+
+
+haveThree([3, 1, 3, 1, 3]) → true
+haveThree([3, 1, 3, 3]) → false
+haveThree([3, 4, 3, 3, 4]) → false
+ */
+
+public boolean haveThree(int[] nums) {
+    if(nums.length<3){
+        return false;
+    }
+    int count=0;
+    for(int i=0;i<nums.length-1;i++){
+        if(nums[i]==3 && nums[i+1]!=3){
+            count++;
+        }
+    }
+    if(nums[nums.length-1]==3 && nums[nums.length-2]!=3){
+        count++;
+    }
+    return count==3;
+}
