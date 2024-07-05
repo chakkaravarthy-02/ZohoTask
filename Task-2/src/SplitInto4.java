fbe4609398ceeba5b38b55317908c235cffbac1c
chakkaravarthy-02 <saranchakkaravarthy@gmail.com>
2024-07-05 16:03:12 +0530

    SplitInto4 updated

--- Task-2/src/SplitInto4.java
+++ Task-2/src/SplitInto4.java
@@ -1,2 +1,16 @@
+
 public class SplitInto4 {
+    public static void main(String[] args){
+        int students=20;
+        int groups=4;
+        int rollNoStart=101;
+
+        for(int i=1;i<=groups;i++){
+            System.out.println("Group "+i);
+            for(int j=rollNoStart;j<rollNoStart+students;j+=groups){
+                System.out.println(j);
+            }
+            rollNoStart++;
+        }
+    }
 }
